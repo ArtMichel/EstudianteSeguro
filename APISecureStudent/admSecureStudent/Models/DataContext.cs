d0e91edac8d343140f02120b085a4bd7d85bc6fb
ArtMichel <a2019116758@estudiantes.upsa.edu.bo>
2020-09-05 11:55:51 -0400

    Se agrego el proyecto de administración MVC

---
+++ APISecureStudent/admSecureStudent/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace admSecureStudent.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<admSecureStudent.Models.Student> Students { get; set; }
+    }
+}
---
+++ APISecureStudent/admSecureStudent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(admSecureStudent.Startup))]
+namespace admSecureStudent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
